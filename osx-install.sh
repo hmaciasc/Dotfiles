a786d847fc433b930e9f0360fd8438a80e591d09
deftbit <hectormaciasc@gmail.com>
2017-01-02 17:45:25 +0000

    osx install script added
    zgen script cleaned

---
+++ osx-install.sh
@@ -0,0 +1,29 @@
+# set default shell to zsh
+chsh -s $(which zsh)
+
+# Install devtools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install things with Homebrew
+brew install nvm yarn mas trash zsh-git-prompt zsh-syntax-highlighting autojump
+brew cask install google-chrome atom hyper rightfont google-play-music-desktop-player dropbox bartender alfred 1password dash kap epichrome istat-menus transmission slack firefox figma vlc ngrok aerial
+
+# Node Stuff
+mkdir ~/.nvm
+nvm install 6.9
+nvm alias default 6.9
+npm i -g np commitizen cz-conventional-changelog hpm-cli
+hpm i hyperterm-material hyperterm-blink hyperterm-tab-icons hyperlinks
+
+# Signin to MAS - mac app store
+mas signin email@mail.com
+
+# Install App Store apps
+mas install 413857545 824183456 824171161 409789998 1106867065 1053031090
+
+# create config files
+echo 'source $HOME/projects/dotfiles/.zshrc' > ~/.zshrc
+cp -f .czrc ~
--- zsh/zgen.sh
+++ zsh/zgen.sh
@@ -1,7 +1,16 @@
 # installation
+echo "Cloning Zgen"
 cd ~
 git clone https://github.com/tarjoilija/zgen.git .zgen
+echo "Finished cloning"
 
 # load zgen
 echo "source "${HOME}/.zgen/zgen.zsh"" >> ~/.zshrc
 cat zgenrc > ~/.zshrc
+
+
+# Copy config
+echo "Importing configuration files"
+cp zgenrc > ~/.zshrc
+cp themes/bullet-train.zsh-theme .zgen/caiogondim/bullet-train-oh-my-zsh-theme-master
+echo "Zgen installed"
